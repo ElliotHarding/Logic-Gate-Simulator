278ca9f3c54b20d059156b856641cc5746304df5
ElliotHarding <elliot.one.harding@gmail.com>
2019-05-05 09:18:59 +0100

    progress

--- Update/Logic_Gate_Simulator/widget_allgates.cpp
+++ Update/Logic_Gate_Simulator/widget_allgates.cpp
@@ -1,14 +1,40 @@
 #include "widget_allgates.h"
 #include "ui_widget_allgates.h"
+#include "dlg_home.h"
 
-Widget_AllGates::Widget_AllGates(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::Widget_AllGates)
+Widget_AllGates::Widget_AllGates(DLG_Home* parent) :
+    QWidget(parent),    
+    ui(new Ui::Widget_AllGates),
+    m_pParent(parent)
 {
     ui->setupUi(this);
 }
-
 Widget_AllGates::~Widget_AllGates()
 {
     delete ui;
+}
+void Widget_AllGates::on_btn_sourceGate_clicked()
+{
+    if(m_pParent)
+        m_pParent->AddGate(new GateToggle());
+}
+void Widget_AllGates::on_btn_notGate_clicked()
+{
+    if(m_pParent)
+        m_pParent->AddGate(new GateNot());
+}
+void Widget_AllGates::on_btn_orGate_clicked()
+{
+    if(m_pParent)
+        m_pParent->AddGate(new GateOr());
+}
+void Widget_AllGates::on_btn_andGate_clicked()
+{
+    if(m_pParent)
+        m_pParent->AddGate(new GateAnd());
+}
+void Widget_AllGates::on_btn_recieverGate_clicked()
+{
+    if(m_pParent)
+        m_pParent->AddGate(new GateReciever());
 }

bfe2b24bb973cadee431c252fb6beb6ea27a3638
elliotharding <elliot.one.harding@gmail.com>
2022-06-09 19:44:44 +0100

    Removed Uni work, thats in master22

--- n0688119_source/Logic_Gate_Simulator/filelocations.h
+++
@@ -1,8 +0,0 @@
-#ifndef FILELOCATIONS_H
-#define FILELOCATIONS_H
-#include <QString>
-
-const static QString c_CustomGatesLocation = "CustomGates/";
-const static QString c_tasksLocation = "Tasks/";
-
-#endif // FILELOCATIONS_H

c0c76452cf7969939df50c35899e7e6598a035cf
elliotharding <elliot.one.harding@gmail.com>
2022-06-10 06:41:56 +0100

    first build

--- Application/Logic_Gate_Simulator/Dialogs/widget_standard.cpp
+++ Application/Logic_Gate_Simulator/Dialogs/widget_standard.cpp
@@ -17,37 +17,37 @@ Widget_Standard::~Widget_Standard()
 void Widget_Standard::on_btn_orGate_clicked()
 {
     if(m_pParent)
-        m_pParent->AddGate(new GateOr());
+        m_pParent->AddGate(new GateOr(0,0));
 }
 void Widget_Standard::on_btn_notGate_clicked()
 {
     if(m_pParent)
-        m_pParent->AddGate(new GateNot());
+        m_pParent->AddGate(new GateNot(0,0));
 }
 void Widget_Standard::on_btn_andGate_clicked()
 {
     if(m_pParent)
-        m_pParent->AddGate(new GateAnd());
+        m_pParent->AddGate(new GateAnd(0,0));
 }
 void Widget_Standard::on_btn_GateTriOr_clicked()
 {
     if(m_pParent)
-        m_pParent->AddGate(new GateTriOr());
+        m_pParent->AddGate(new GateTriOr(0,0));
 }
 void Widget_Standard::on_btn_GateTriAnd_clicked()
 {
     if(m_pParent)
-        m_pParent->AddGate(new GateTriAnd());
+        m_pParent->AddGate(new GateTriAnd(0,0));
 }
 
 void Widget_Standard::on_btn_GateEor_clicked()
 {
     if(m_pParent)
-        m_pParent->AddGate(new GateEor());
+        m_pParent->AddGate(new GateEor(0,0));
 }
 
 void Widget_Standard::on_btn_GateTriEor_clicked()
 {
     if(m_pParent)
-        m_pParent->AddGate(new GateTriEor());
+        m_pParent->AddGate(new GateTriEor(0,0));
 }

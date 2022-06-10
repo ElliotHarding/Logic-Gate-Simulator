d043a682c35d95adade5ad9469c4b0898760f010
elliotharding <elliot.one.harding@gmail.com>
2022-06-10 05:34:09 +0100

    gate tri eor

--- Application/Logic_Gate_Simulator/GameObjects/gatetrieor.cpp
+++ Application/Logic_Gate_Simulator/GameObjects/gatetrieor.cpp
@@ -1,15 +1,33 @@
 #include "gatetrieor.h"
 
-GateTriEor::GateTriEor(id inA, id inB, id inC, id out) :
-    Gate::Gate(GATE_TRI_EOR, GateTriEorWidth, GateTriEorHeight, std::string(":/Resources/Gates/gate-tri-eor.png").c_str()),
-    m_pInputA(this, InputNode, inA),
-    m_pInputB(this, InputNode, inB),
-    m_pInputC(this, InputNode, inC),
-    m_output(this, OutputNode, out)
+namespace Settings
+{
+const uint GateTriEorHeight = 50;
+const uint GateTriEorWidth = 100;
+
+const int NodeOffsetX_a = -5;
+const int NodeOffsetY_a = (GateTriEorHeight/2) - 11;
+
+const int NodeOffsetX_b = -5;
+const int NodeOffsetY_b = GateTriEorHeight/2;
+
+const int NodeOffsetX_c = -5;
+const int NodeOffsetY_c = GateTriEorHeight - 13;
+
+const int NodeOffsetX_d = GateTriEorWidth + 5;
+const int NodeOffsetY_d = (GateTriEorHeight/2);
+}
+
+GateTriEor::GateTriEor(const int &x, const int &y, const id &inA, const id &inB, const id &inC, const id &out) :
+    Gate::Gate(GATE_TRI_EOR, x, y, Settings::GateTriEorWidth, Settings::GateTriEorHeight, std::string(":/Resources/Gates/gate-tri-eor.png").c_str()),
+    m_pInputA(new Node(this, Settings::NodeOffsetX_a, Settings::NodeOffsetY_a, InputNode, inA)),
+    m_pInputB(new Node(this, Settings::NodeOffsetX_b, Settings::NodeOffsetY_b, InputNode, inB)),
+    m_pInputC(new Node(this, Settings::NodeOffsetX_c, Settings::NodeOffsetY_c, InputNode, inC)),
+    m_pOutput(new Node(this, Settings::NodeOffsetX_d, Settings::NodeOffsetY_d, OutputNode, out))
 {
     m_nodes.push_back(m_pInputA);
     m_nodes.push_back(m_pInputB);
-    m_nodes.push_back(&m_pInputC);
+    m_nodes.push_back(m_pInputC);
     m_nodes.push_back(m_pOutput);
 }
 
@@ -18,6 +36,7 @@ void GateTriEor::UpdateOutput()
     const bool sum = m_pInputA->value() ^ m_pInputB->value() ^ m_pInputC->value();
     m_pOutput->setValue(sum);
 
+    //Todo : what is this?
     if (m_pInputA->value() & m_pInputB->value() & m_pInputC->value())
     {
 
@@ -28,23 +47,9 @@ void GateTriEor::UpdateOutput()
     }
 }
 
-void GateTriEor::setPosition(int x, int y)
-{
-    GameObject::setPosition(x,y);
-
-    m_pInputA.setPosition(m_layout.x() + m_pInputA_OFFSET_X, m_layout.y() + m_pInputA_OFFSET_Y);
-    m_pInputB.setPosition(m_layout.x() + m_pInputB_OFFSET_X, m_layout.y() + m_pInputB_OFFSET_Y);
-    m_pInputC.setPosition(m_layout.x() + m_pInputC_OFFSET_X, m_layout.y() + m_pInputC_OFFSET_Y);
-    m_output.setPosition(m_layout.x() + M_OUTPUT_OFFSET_X, m_layout.y() + M_OUTPUT_OFFSET_Y);
-}
-
 Gate *GateTriEor::Clone()
 {
-    GateTriEor* clone = new GateTriEor();
-
-    //Clone position
-    QPoint pos = position();
-    clone->setPosition(pos.x(), pos.y());
+    GateTriEor* clone = new GateTriEor(m_geometry.x(), m_geometry.y(), m_pInputA->id(), m_pInputB->id(), m_pInputC->id(), m_pOutput->id());
 
     //Clone nodes
     clone->m_pInputA = m_pInputA;
--- Application/Logic_Gate_Simulator/GameObjects/gatetrieor.h
+++ Application/Logic_Gate_Simulator/GameObjects/gatetrieor.h
@@ -3,36 +3,20 @@
 
 #include "gate.h"
 
-#define GateTriEorHeight 50
-#define GateTriEorWidth 100
-
 class GateTriEor : public Gate
 {
 public:
-    GateTriEor(id inA = idGenerator(), id inB = idGenerator(), id inC = idGenerator(), id out = idGenerator());
+    GateTriEor(const int &x, const int &y, const id& inA = idGenerator(), const id& inB = idGenerator(), const id& inC = idGenerator(), const id& out = idGenerator());
 
     virtual void UpdateOutput() override;
-    virtual void setPosition(int x, int y) override;
-
     virtual Gate* Clone() override;
 
 protected:
-
-    const int m_pInputA_OFFSET_X = -5;
-    const int m_pInputA_OFFSET_Y = (GateTriEorHeight/2) - 11;
+    ///Nodes
     Node* m_pInputA;
-
-    const int m_pInputB_OFFSET_X = -5;
-    const int m_pInputB_OFFSET_Y = GateTriEorHeight/2;
     Node* m_pInputB;
-
-    const int m_pInputC_OFFSET_X = -5;
-    const int m_pInputC_OFFSET_Y = GateTriEorHeight - 13;
     Node* m_pInputC;
-
-    const int M_OUTPUT_OFFSET_X = GateTriEorWidth + 5;
-    const int M_OUTPUT_OFFSET_Y = (GateTriEorHeight/2);
-    Node m_output;
+    Node* m_pOutput;
 };
 
 #endif // GATETRIEOR_H

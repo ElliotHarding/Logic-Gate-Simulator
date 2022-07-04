a6d9b18604fef685011cb29249e6f8e4ffe74226
elliotharding <elliot.one.harding@gmail.com>
2022-07-04 16:12:17 +0100

    Working on truth table

--- Application/Logic_Gate_Simulator/Dialogs/dlg_truthtable.cpp
+++ Application/Logic_Gate_Simulator/Dialogs/dlg_truthtable.cpp
@@ -12,3 +12,37 @@ DLG_TruthTable::~DLG_TruthTable()
 {
     delete ui;
 }
+
+void DLG_TruthTable::open(const TruthTable& truthTable)
+{
+    QDialog::open();
+}
+
+void DLG_TruthTable::open()
+{
+    QDialog::open();
+}
+
+////////////////////////////////////////////////////////
+/// \brief BinaryLabel::BinaryLabel
+/// \param parent
+///
+BinaryLabel::BinaryLabel(QWidget* parent, const bool& value, const bool& editDisabled) : QLabel(parent),
+    m_bEditDisabled(editDisabled)
+{
+    setText(value ? "1" : "0");
+}
+
+bool BinaryLabel::value()
+{
+    return text() == "1";
+}
+
+void BinaryLabel::mousePressEvent(QMouseEvent* event)
+{
+    if(!m_bEditDisabled)
+    {
+        setText(value() ? "0" : "1");
+    }
+    QLabel::mousePressEvent(event);
+}
--- Application/Logic_Gate_Simulator/Dialogs/dlg_truthtable.h
+++ Application/Logic_Gate_Simulator/Dialogs/dlg_truthtable.h
@@ -1,11 +1,16 @@
 #ifndef DLG_TRUTHTABLE_H
 #define DLG_TRUTHTABLE_H
 
+#include "truthtable.h"
+
 #include <QDialog>
+#include <QLabel>
 
 namespace Ui {
 class DLG_TruthTable;
 }
+
+class BinaryLabel;
 
 class DLG_TruthTable : public QDialog
 {
@@ -15,8 +20,32 @@ public:
     explicit DLG_TruthTable(QWidget *parent = nullptr);
     ~DLG_TruthTable();
 
+    //Open to display truth table
+    void open(const TruthTable& truthTable);
+
+    //Open to create new truth table
+    void open();
+
 private:
     Ui::DLG_TruthTable *ui;
+
+    std::vector<std::vector<BinaryLabel*>> m_resultLabels;
+    std::vector<QLabel*> m_labelBin;
+};
+
+class BinaryLabel : public QLabel
+{
+    Q_OBJECT
+
+public:
+    BinaryLabel(QWidget* parent, const bool& value = false, const bool& editDisabled = false);
+
+    bool value();
+
+protected:
+    void mousePressEvent(QMouseEvent* event) override;
+
+    bool m_bEditDisabled = false;
 };
 
 #endif // DLG_TRUTHTABLE_H

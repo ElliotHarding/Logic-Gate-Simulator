064d2d8a3927b88bb6884941fe7da3bcb0e16d0e
elliotharding <elliot.one.harding@gmail.com>
2022-07-28 21:00:37 +0100

    wip - still broken - progress

--- Application/Logic_Gate_Simulator/truthtable.cpp
+++ Application/Logic_Gate_Simulator/truthtable.cpp
@@ -78,47 +78,15 @@ ExpressionCalculatorResult BooleanExpressionCalculator::truthTableToBooleanExpre
         return INVALID_INPUT_EXPRESSIONS;
     }
 
-    const uint numOutputs = truthTable.outValues[0].size();
-    const uint numRuns = truthTable.inValues.size();
-
-    for(uint iOutput = 0; iOutput < numOutputs; iOutput++)
-    {
-        BooleanExpression expression;
-        expression.resultLetter = char(Settings::IntEndAlphabet-iOutput);
-
-        for(uint iTableRun = 0; iTableRun < numRuns; iTableRun++)
-        {
-            if(truthTable.outValues[iTableRun][iOutput])
-            {
-                if(expression.letters.size() != 0)
-                {
-                    expression.letters.push_back('+');
-                    expression.inverted.push_back(false);
-                }
-
-                for(uint iInput = 0; iInput < truthTable.inValues[iTableRun].size(); iInput++)
-                {
-                    if(expression.letters.size() != 0 && iInput != 0)
-                    {
-                        expression.letters.push_back('.');
-                        expression.inverted.push_back(false);
-                    }
-
-                    expression.letters.push_back(char(Settings::IntStartAlphabet+iInput));
-                    expression.inverted.push_back(!(truthTable.inValues[iTableRun][iInput]));
-                }
-            }
-        }
-
-        expressions.push_back(expression);
-    }
-
     //Todo ~ simplify
 
     // - Basic rule simplification
     // - Karnaugh maps simplification
     // - Quine-McCluskey algorithm
 
+
+    const uint numOutputs = truthTable.outValues[0].size();
+    const uint numRuns = truthTable.inValues.size();
     const uint numInputs = truthTable.inValues[0].size();
 
     QString dontCaresMask = "";

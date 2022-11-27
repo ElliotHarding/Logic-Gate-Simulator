9918338776ab39289ca7ba60c89c17454d50c3ed
elliotharding <elliot.one.harding@gmail.com>
2022-11-27 06:38:27 +0000

    wip - good work

--- Application/Logic_Gate_Simulator/karnaughmap.cpp
+++ Application/Logic_Gate_Simulator/karnaughmap.cpp
@@ -1,8 +1,29 @@
 #include "karnaughmap.h"
 #include <cmath>
+#include "truthtable.h"
 
 bool KarnaughMap::init(std::vector<char> inputLetters)
 {
-    int xInputsCount = std::floor(inputLetters.size() / 2);
-    int yInputsCount = inputLetters.size() - xInputsCount;
+    const int numInputLetters = inputLetters.size();
+    const int xInputsCount = std::floor(numInputLetters / 2);
+    const int count = pow(2, numInputLetters);
+
+    //Generate input values
+    TruthTable temp;
+    for (int iRun = 0; iRun < count; iRun++)
+    {
+        std::vector<bool> inputValues = temp.genInputs(iRun, numInputLetters);
+        for(int iInputValue = 0; iInputValue < numInputLetters; iInputValue++)
+        {
+            std::pair<char, bool> input(inputLetters[iInputValue], inputValues[iInputValue]);
+            if(iInputValue < xInputsCount)
+            {
+                xInputs.push_back(input);
+            }
+            else
+            {
+                yInputs.push_back(input);
+            }
+        }
+    }
 }

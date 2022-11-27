10212c8f70827c7791ed77bfccf30b8f77761869
elliotharding <elliot.one.harding@gmail.com>
2022-11-27 08:06:49 +0000

    wip - a bit o progress

--- Application/Logic_Gate_Simulator/karnaughmap.cpp
+++ Application/Logic_Gate_Simulator/karnaughmap.cpp
@@ -8,8 +8,34 @@ bool KarnaughMap::init(const std::vector<char>& inLetters, const char& cOutputLe
     inputLetters = inLetters;
     const int numInputLetters = inputLetters.size();
     const int xInputsCount = std::floor(numInputLetters / 2);
-    const int count = pow(2, numInputLetters);
+    const int yInputsCount = inputLetters.size() - xInputsCount;
 
+    const int xRowLen = pow(2, xInputsCount);
+    const int yColLen = pow(2, yInputsCount);
+
+    for(int x = 0; x < xRowLen; x++)
+    {
+        std::vector<std::pair<char, bool>> colInputs;
+        std::vector<bool> inValues = TruthTable::genInputValues(x, xInputsCount);
+        for(int iInput = 0; iInput < inValues.size(); iInput++)
+        {
+            std::pair<char, bool> colInput(inLetters[iInput], inValues[iInput]);
+            colInputs.push_back(colInput);
+        }
+        xInputs.push_back(colInputs);
+    }
+
+    for(int y = 0; y < yColLen; y++)
+    {
+        std::vector<std::pair<char, bool>> rowInputs;
+        std::vector<bool> inValues = TruthTable::genInputValues(y, xInputsCount);
+        for(int iInput = 0; iInput < inValues.size(); iInput++)
+        {
+            std::pair<char, bool> rowInput(inLetters[iInput+xInputsCount], inValues[iInput]);
+            rowInputs.push_back(rowInput);
+        }
+        yInputs.push_back(rowInputs);
+    }
 
 
     return true;
